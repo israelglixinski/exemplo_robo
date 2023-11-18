93ac1fc5ff3e3d6c5e959260216fae08167360e5
Israel Glixinski <israel_glixinski@hotmail.com>
2023-11-18 16:10:18 -0300

    atualização importante no abs path

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 from time import sleep
+import os
+
+
+my_path = str(os.path.abspath(__file__)).replace(os.path.basename(__file__),'')
+print(my_path)
 
 
 while True:
-    with open('./logs/log.txt','a') as arquivo:
+    with open(f'{my_path}logs/log.txt','a') as arquivo:
         print(datetime.now(),file=arquivo)
     sleep(3)
-
-
-
 
